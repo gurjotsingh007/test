bec46a7159d13a3bb6fa271c9719eb1f2d34d517
Gurjot Singh <101056456+gurjotsingh007@users.noreply.github.com>
2023-11-08 01:26:25 +0530

    1.1

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,41 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-const connectDatabase = require("./config/database");
+const express = require('express');
+const app = require('./app');
+const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
+const path = require('path');
+
+dotenv.config({ path: 'backend/config/config.env' });
 
 async function startServer() {
   try {
-    dotenv.config({ path: "backend/config/config.env" });
     await connectDatabase();
 
-    const port = process.env.PORT;
+    const __dirname = path.resolve();
 
-    const server = app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+      app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')
+      ));
+    } else {
+      app.get('/', (req, res) => {
+        res.send('API is running....');
+      });
+
+      const server = app.listen(process.env.PORT, process.env.HOST, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+      });
+    }
 
     // Handling unhandled exceptions
-    process.on("uncaughtException", (err) => {
+    process.on('uncaughtException', (err) => {
       console.error(`Uncaught Exception: ${err.message}`);
       shutdown(server);
     });
 
     // Handling unhandled promise rejections
-    process.on("unhandledRejection", (err) => {
+    process.on('unhandledRejection', (err) => {
       console.error(`Unhandled Promise Rejection: ${err.message}`);
       shutdown(server);
     });
@@ -28,6 +43,7 @@ async function startServer() {
     console.error(`Error during server start: ${err.message}`);
   }
 }
+
 function shutdown(server) {
   console.log('Shutting Down the Server');
   server.close(() => {
